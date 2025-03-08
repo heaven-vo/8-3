4c03c4ba4d78465a2857c53989972afc1c6e97ff
heaven-vo <congvo.heaven@gmail.com>
2025-03-08 18:45:43 +0700

    final

--- src/components/MessageCard.js
+++ src/components/MessageCard.js
@@ -181,12 +181,12 @@ const CustomHeart = ({ color }) => (
 );
 
 const messages = [
-    "Gửi đến người phụ nữ tuyệt vời nhất trong cuộc đời anh, em mang đến niềm vui và hạnh phúc cho mỗi ngày.",
-    "Nụ cười của em thắp sáng thế giới của anh, và tình yêu của em lấp đầy trái tim anh bằng sự ấm áp.",
-    "Vào ngày đặc biệt này, anh muốn em biết em có ý nghĩa như thế nào đối với anh.",
-    "Em không chỉ là bạn gái của anh, em là bạn thân nhất của anh, là người bạn tâm giao và là tất cả của anh.",
-    "Anh trân trọng từng khoảnh khắc chúng ta bên nhau, và anh mong muốn tạo ra nhiều kỷ niệm đẹp hơn nữa.",
-    "Chúc mừng Ngày Phụ nữ, tình yêu của anh! Em xứng đáng có được tất cả hạnh phúc trên thế giới này."
+    "Gửi đến người con gái tuyệt vời trên thế giới này, cậu mang đến niềm vui và hạnh phúc cho thế giới này.",
+    "Mong rằng mỗi này trôi qua, cậu không chỉ nhận được sự quan tâm, trân trọng mà còn luôn tỏa sáng theo cách riêng của mình.",
+    "Vào ngày đặc biệt này, chúc cậu luôn vui vẻ, gặp nhiều may mắn, mỗi ngày đều cười tươi.",
+    "Chúc cậu ngày càng xinh đẹp, giỏi giang, cuộc sống đong đầy hạnh phúc.",
+    "Chúc mừng Ngày Phụ nữ!",
+    "Hãy mãi là bông hoa rực rỡ nhất, xứng đáng với những điều tuyệt với nhất trong cuộc sống",
 ];
 
 // Then update the MessageCard component to accept and pass the prop:
@@ -415,7 +415,7 @@ const MessageCard = ({ marginBottom }) => {
                                 repeatType: "reverse"
                             }}
                         >
-                            I Love You
+                            Nguyễn Thị Như Ý 
                         </motion.div>
                     </FinalMessage>
                 </FullscreenOverlay>
